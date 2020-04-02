3c833b3180b9aa92ad7e0aa300b7cf91ce280bb8
liuxiaoming <384276224@qq.com>
2020-04-02 21:49:43 +0800

    安装测试axios

--- mango-ui/src/views/Home.vue
+++ mango-ui/src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="page">
         <h2>Home Page</h2>
+        <el-button type="primary" @click="testAxios()">测试axios调用</el-button>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    name: 'Home'    
+    name: 'Home',
+    methods:{
+        testAxios(){
+            axios.get('http://localhost:8080')
+                .then(res=>{
+                    alert(res.data);
+                })
+        }
+    }    
 }
 </script>
