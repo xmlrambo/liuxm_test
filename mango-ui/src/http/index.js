398665a49fea19f0460882b1d52595362344a24a
liuxiaoming <384276224@qq.com>
2020-04-03 22:28:02 +0800

    封装axios

--- mango-ui/src/http/index.js
+++ mango-ui/src/http/index.js
@@ -1,1 +1,20 @@
-//将axios封装成插件
+//将axios封装成插件 导入所有接口
+import api from './api'
+
+const install = Vue =>{
+  if(install.installed)
+    return;
+  
+  install.installed=true;
+
+  Object.defineProperties(Vue.prototype, {
+    //此处挂载在Vue原型的$api对象上
+    $api:{
+      get(){
+        return api
+      }
+    }
+  })
+}
+
+export default install
